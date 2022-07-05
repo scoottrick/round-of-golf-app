319077e5efdc7e78020445d78a188c3d8bca55d0
Scott <csschapker94@gmail.com>
2022-07-04 23:56:48 -0500

    Adds react-icons

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { FaBeer } from 'react-icons/fa';
 
 const HomePage = () => {
-  return <h1 className="uk-text-lead">Home</h1>;
+  return (
+    <h1 className="uk-text-lead">
+      Lets go for a <FaBeer />?
+    </h1>
+  );
 };
 export default HomePage;
