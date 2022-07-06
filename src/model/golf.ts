c82fba09d353d5ec4aff5c34bdf7417815bff7f6
Scott <csschapker94@gmail.com>
2022-07-05 23:54:46 -0500

    Adds more consistent golf-related methods

--- src/model/golf.ts
+++ src/model/golf.ts
@@ -1,17 +1,27 @@
 import { v4 as uuid } from 'uuid';
+
+type EntityId = string;
+
+const entityId = () => uuid() as EntityId;
+const arrayOfN = (n: number) => [...new Array(n)];
 export interface Golfer {
-  id: string;
+  id: EntityId;
   name: string;
   scores: number[];
 }
 
+export interface GolfHole {
+  par: number;
+}
+
 export interface GolfCourse {
+  id: EntityId;
   name: string;
   holes: { par: number }[];
 }
 
 export interface GolfRound {
-  id: string;
+  id: EntityId;
   date: number;
   completed: boolean;
   golfers: Golfer[];
@@ -21,7 +31,7 @@ export interface GolfRound {
 export const createGolfers = (names: string[]) => {
   const validNames = names.filter(name => name.trim().length);
   const golfers: Golfer[] = validNames.map(name => ({
-    id: uuid(),
+    id: entityId(),
     name: name,
     scores: [],
   }));
@@ -29,15 +39,16 @@ export const createGolfers = (names: string[]) => {
 };
 
 export const createGolfCourse = (name: string, holeCount: number) => {
-  const holes = [...new Array(holeCount)].map(() => ({ par: 0 }));
-  const course: GolfCourse = { name, holes };
+  const id = entityId();
+  const holes = arrayOfN(holeCount).map(() => ({ par: 0 }));
+  const course: GolfCourse = { id, name, holes };
   return course;
 };
 
 export const createGolfRound = (golfers: Golfer[], course: GolfCourse) => {
   const emptyScores = course.holes.map(() => 0);
   const newRound: GolfRound = {
-    id: uuid(),
+    id: entityId(),
     date: Date.now(),
     completed: false,
     golfers: golfers.map(g => ({ ...g, scores: emptyScores })),
@@ -47,6 +58,31 @@ export const createGolfRound = (golfers: Golfer[], course: GolfCourse) => {
 };
 
 export class GolfUtils {
+  static createGolfers(names: string[]): Golfer[] {
+    return names.map(name => GolfUtils.createGolfer(name));
+  }
+  private static createGolfer(name: string): Golfer {
+    return {
+      id: entityId(),
+      name: name,
+      scores: [],
+    };
+  }
+
+  static newCourse(name: string, holes: GolfHole[]): GolfCourse {
+    return {
+      id: entityId(),
+      name: name || `${holes.length} Holes`,
+      holes: holes,
+    };
+  }
+  static newPar3Course(name: string, holeCount: number): GolfCourse {
+    return GolfUtils.newCourse(
+      name,
+      arrayOfN(holeCount).map(() => ({ par: 3 }))
+    );
+  }
+
   static calcStrokes(golfer: Golfer): number {
     return golfer.scores
       .filter(s => s > 0)
@@ -57,9 +93,9 @@ export class GolfUtils {
     const golfers = [...round.golfers];
     const initialGolfer = golfers.splice(0, 1)[0];
     let leaders = [initialGolfer];
-    let minScore = this.calcStrokes(initialGolfer);
+    let minScore = GolfUtils.calcStrokes(initialGolfer);
     for (let golfer of round.golfers) {
-      const score = this.calcStrokes(golfer);
+      const score = GolfUtils.calcStrokes(golfer);
       if (score === minScore) {
         leaders = [...leaders, golfer];
         break;
