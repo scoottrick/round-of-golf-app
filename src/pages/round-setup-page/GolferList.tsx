1ad32151d934e86093234782b12b66070b873b2c
Scott <csschapker94@gmail.com>
2022-07-06 18:50:52 -0500

    Adds basic styling and functionality for the gofler list

--- src/pages/round-setup-page/GolferList.tsx
+++ src/pages/round-setup-page/GolferList.tsx
@@ -1,4 +1,22 @@
-import React, { FC } from 'react';
+import React, { FC, ReactElement, ReactNode } from 'react';
+import { GrAdd, GrClose } from 'react-icons/gr';
+import { classNames } from '../../model/utils';
+
+interface IconButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+}
+const IconButton: FC<IconButtonProps> = ({ className, children, ...props }) => {
+  return (
+    <button
+      type="button"
+      className={classNames(['icon-btn rounded', className])}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
 
 interface Props {
   names: string[];
@@ -9,20 +27,43 @@ const GolferList: FC<Props> = ({ names, namesUpdated }) => {
     names.splice(index, 1, value);
     namesUpdated([...names]);
   };
+  const removeClicked = (index: number) => {
+    names.splice(index, 1);
+    namesUpdated([...names]);
+  };
+  const addClicked = () => {
+    namesUpdated([...names, '']);
+  };
   const nameItems = names.map((name, i) => (
-    <li key={i}>
+    <li
+      key={i}
+      className="flex flex-row justify-between align-middle mb-2 last:mb-0"
+    >
       <input
+        className={classNames(['border-b-2 text-md py-1 flex-1'])}
         type="text"
         value={name}
         onChange={e => nameChanged(e.target.value, i)}
       />
+      <IconButton
+        data-app-hidden={names.length <= 1}
+        className="ml-4"
+        onClick={() => removeClicked(i)}
+      >
+        <GrClose />
+      </IconButton>
     </li>
   ));
   return (
-    <div>
-      <h1 className="text-2xl">Golfers</h1>
-      <ul>{nameItems}</ul>
-    </div>
+    <>
+      <div className="flex flex-row justify-between mb-8">
+        <h1 className="text-2xl flex-1">Golfers</h1>
+        <IconButton onClick={() => addClicked()}>
+          <GrAdd />
+        </IconButton>
+      </div>
+      <ul className="mb-4 last:mb-0">{nameItems}</ul>
+    </>
   );
 };
 export default GolferList;
